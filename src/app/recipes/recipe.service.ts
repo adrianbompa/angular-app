3a28e624acc8c2ae37af94684f44b37253f88d1b
Adrian Bompa <abompa@Adrians-MacBook-Pro.local>
2019-05-17 10:23:50 +0100

    update

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,17 +1,36 @@
 import {Recipe} from './recipe.model';
-import {EventEmitter} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
+  constructor(private slService: ShoppingListService) {
+
+  }
+
   private recipes: Recipe[] = [
     new Recipe('A test recipe', ' This is simply a test', 'https://proxy.duckduckgo.com/' +
-      'iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2017%2F09%2F12%2F21%2F43%2Fpotato-salad-2743776_640.jpg&f=1'),
+      'iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2017%2F09%2F12%2F21%2F43%2Fpotato-salad-2743776_640.jpg&f=1',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20),
+      ]),
     new Recipe('Another test recipe', ' This is simply a test', 'https://proxy.duckduckgo.com/' +
-      'iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2017%2F09%2F12%2F21%2F43%2Fpotato-salad-2743776_640.jpg&f=1')
+      'iu/?u=https%3A%2F%2Fcdn.pixabay.com%2Fphoto%2F2017%2F09%2F12%2F21%2F43%2Fpotato-salad-2743776_640.jpg&f=1',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+      ])
   ];
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -16,5 +16,10 @@ export class ShoppingListService {
     this.ingredients.push(ingredient);
     this.ingredientsChanges.emit(this.ingredients.slice());
   }
+
+  addIngredients(ingredients: Ingredient[]) {
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanges.emit(this.ingredients.slice());
+  }
 }
 
